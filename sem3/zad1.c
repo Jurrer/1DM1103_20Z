1fefe879f635bb511af578c9887e17148b9c7abf
Jerzy Kirczuk <72750942+Jurrer@users.noreply.github.com>
2020-10-22 23:44:42 +0200

    Add files via upload

---
+++ sem3/zad1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[]){
+
+float x, y;
+
+
+x = atof(argv[1]);
+y=x*x;
+
+    printf("%s do kwadratu to %f\n", argv[1], y);
+
+return 0;
+}
